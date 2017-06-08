3ee8820947840feab554b01357ab906b6555942e
Michael Dawson <mdawson@devrus.com>
2017-06-07 22:17:40 -0400

    src: add LD_LIBRARY_PATH back in

--- data/usr/bin/start-mqtt.sh
+++ data/usr/bin/start-mqtt.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export CURRENT_DIR=$(dirname "$0")
+export LD_LIBRARY_PATH=/media/mmcblk0p2/data/usr/lib:$LD_LIBRARY_PATH
 source $CURRENT_DIR/snapshot-config.sh
 cd $CURRENT_DIR
 $CURRENT_DIR/PIWCmain "$MQTT_TOPIC_PREFIX/$CAMERA_ID/capture" "$MQTT_TOPIC_PREFIX/$CAMERA_ID/newpicture" "$MQTT_URL" "$CERTS_DIR"
